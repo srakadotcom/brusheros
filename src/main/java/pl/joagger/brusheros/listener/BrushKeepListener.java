9b134d910dabd93bb3e6eb3e944d2994bc1069e4
Memexurer <{meme}+xurer@testzeber.pl>
2022-01-13 22:35:30 +0100

    Inventory click fixes

--- src/main/java/pl/joagger/brusheros/listener/BrushKeepListener.java
+++ src/main/java/pl/joagger/brusheros/listener/BrushKeepListener.java
@@ -1,17 +1,21 @@
 package pl.joagger.brusheros.listener;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import pl.joagger.brusheros.BrushItem;
 import pl.joagger.brusheros.BrushPluginConfiguration;
 
 public class BrushKeepListener implements Listener {
+
+    private static final String PERMISSION = "brusheros.drop";
+
     private final BrushPluginConfiguration configuration;
 
     public BrushKeepListener(BrushPluginConfiguration configuration) {
@@ -20,8 +24,12 @@ public class BrushKeepListener implements Listener {
 
     @EventHandler
     private void onDrop(PlayerDropItemEvent event) {
+        if (event.getPlayer().hasPermission(PERMISSION)) {
+            return;
+        }
+
         BrushItem item = configuration.findByItemStack(event.getItemDrop().getItemStack());
-        if (item != null && !event.getPlayer().hasPermission("brusheros.drop")) {
+        if (item != null) {
             event.setCancelled(true);
         }
     }
@@ -29,15 +37,56 @@ public class BrushKeepListener implements Listener {
 
     @EventHandler
     private void onItemClickMove(InventoryClickEvent event) {
-        if (!isAllowed(event.getClickedInventory(), event.getWhoClicked().getInventory()))
-            if (configuration.findByItemStack(event.getCursor()) != null || configuration.findByItemStack(event.getCurrentItem()) != null)
+        if (event.getWhoClicked().hasPermission(PERMISSION)) {
+            return;
+        }
+
+        if (isForbidden(event.getClickedInventory(), event.getWhoClicked().getInventory())) {
+            if (configuration.findByItemStack(event.getCursor()) != null
+                    || configuration.findByItemStack(event.getCurrentItem()) != null) {
                 event.setCancelled(true);
+            }
+        }
     }
 
-    private boolean isAllowed(Inventory source, Inventory destination) {
-        if (destination.getType() == InventoryType.ENDER_CHEST)
-            return true;
+    @EventHandler
+    private void onItemClickMove(InventoryDragEvent event) {
+        if (event.getWhoClicked().hasPermission(PERMISSION)) {
+            return;
+        }
 
-        return source == destination;
+        if (isForbidden(event.getInventory(), event.getWhoClicked().getInventory())) {
+            if (configuration.findByItemStack(event.getCursor()) != null ||
+                    configuration.findByItemStack(event.getOldCursor()) != null) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    private void onHotbarMove(InventoryClickEvent event) {
+        if (event.getWhoClicked().hasPermission(PERMISSION)) {
+            return;
+        }
+
+        if (event.getAction() != InventoryAction.HOTBAR_MOVE_AND_READD &&
+                event.getAction() != InventoryAction.HOTBAR_SWAP) {
+            return;
+        }
+
+        if (isForbidden(event.getClickedInventory(), event.getWhoClicked().getInventory())) {
+            ItemStack itemStack = event.getWhoClicked().getInventory().getItem(event.getHotbarButton());
+            if (itemStack != null && configuration.findByItemStack(itemStack) != null) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    private boolean isForbidden(Inventory source, Inventory destination) {
+        if (destination.getType() == InventoryType.ENDER_CHEST) {
+            return false;
+        }
+
+        return source != destination;
     }
 }
